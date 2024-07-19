5f15e6f84c5de6adf00e0fac422f085aaee6bc02
v1ll4n <v1ll4n.a5k@gmail.com>
2024-07-19 12:04:15 +0800

    fix controller

--- java-realworld/sample/src/main/java/com/example/demo/controller/fastjsondemo/FastJSONDemoController.java
+++ java-realworld/sample/src/main/java/com/example/demo/controller/fastjsondemo/FastJSONDemoController.java
@@ -2,19 +2,17 @@ package com.example.demo.controller.fastjsondemo;
 
 import com.alibaba.fastjson.JSON;
 import org.apache.ibatis.annotations.Param;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/fastjson")
 public class FastJSONDemoController {
     @GetMapping("/fromId")
-    public String loadFromParam(@RequestParam(name = "id") String id) {
+    public ResponseEntity<Object> loadFromParam(@RequestParam(name = "id") String id) {
         // This is a FASTJSON Vuln typically.
         Object anyJSON = JSON.parse(id);
-        return JSON.toJSONString(anyJSON);
+        return ResponseEntity.ok(anyJSON);
     }
 }
 
