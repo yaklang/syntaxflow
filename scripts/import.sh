e3b901a182023e37124d1d63e7505445d14dfd22
VillanCh <188228725@qq.com>
2024-07-20 23:59:52 +0800

    add some details

--- scripts/import.sh
+++ scripts/import.sh
@@ -11,17 +11,10 @@ fi
 
 # 读取项目名并执行相应的操作
 while IFS= read -r PROJECT; do
-    echo "Running YAK SSA for project $PROJECT"
-    yak ssa -t "./$PROJECT" -p "$PROJECT"
+    echo "Running Save SyntaxFlow for project $PROJECT"
+    yak ssf -f "./$PROJECT/sample" -r "$PROJECT"
     if [ $? -ne 0 ]; then
         echo "Error running YAK SSA for $PROJECT"
-        exit 1
-    fi
-
-    echo "Running YAK SF for project $PROJECT"
-    yak sf -p "$PROJECT" "$PROJECT"
-    if [ $? -ne 0 ]; then
-        echo "Error running YAK SF for $PROJECT"
         exit 1
     fi
 
